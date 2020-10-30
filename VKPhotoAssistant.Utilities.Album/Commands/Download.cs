7e8ceac8949122310608dae1d913ee5c8e49fcd0
fman42 <igrok5554@gmail.com>
2020-10-30 16:05:53 +0300

    Changed Download.cs

--- VKPhotoAssistant.Utilities.Album/Commands/Download.cs
+++ VKPhotoAssistant.Utilities.Album/Commands/Download.cs
@@ -7,51 +7,55 @@ using System.Threading.Tasks;
 using VkNet;
 using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
-using VKPhotoAssistant.Storage;
-using VKPhotoAssistant.Interfaces.Utility;
 using VkNet.Model.Attachments;
 using VkNet.Model.RequestParams;
+using VkNet.Utils;
 using VKPhotoAssistant.Interfaces.Storage;
+using VKPhotoAssistant.Interfaces.Utility;
+using VKPhotoAssistant.Storage;
 using VKPhotoAssistant.Utilities.Album.Options;
 using VKPhotoAssistant.Utilities.Base;
+
 namespace VKPhotoAssistant.Utilities.Album.Commands
 {
     internal class Download : BaseCommandParser<DownloadOptions>, ICommand
     {
+        #region Vars
         private VkApi API { get; set; } = new VkApi();
 
         private DownloadOptions DownloadOptions { get; set; }
 
         private WebClient Client { get; } = new WebClient();
 
-        private JsonStorage Storage { get; }
-
-        public Download() => Storage = JsonStorage.GetInstance();
+        private MainStorage Storage { get; } = JsonStorage.GetInstance().Read();
+        #endregion
 
         #region Methods
-        public async Task ExecuteAsync(IEnumerable<string> args) => TryParseAsync(args,
-            async (options) => {
-                MainStorage storage = Storage.Read();
+        public async Task ExecuteAsync(IEnumerable<string> args) => TryParseAsync(args, Action);
 
-                if (!string.IsNullOrEmpty(storage.CurrentVKToken))
-                {
-                    API.Authorize(new ApiAuthParams()
-                    {
-                        AccessToken = storage.CurrentVKToken
-                    });
-
-                    DownloadOptions = options;
-                    DownloadAlbum();
-                }
+        private Task Action(DownloadOptions options)
+        {
+            if (string.IsNullOrEmpty(Storage.CurrentVKToken))
+            {
+                Console.WriteLine("Токен не установлен");
+                return Task.CompletedTask;
             }
-        );
+
+            API.Authorize(new ApiAuthParams()
+            {
+                AccessToken = Storage.CurrentVKToken
+            });
+
+            DownloadOptions = options;
+            DownloadAlbum();
+
+            return Task.CompletedTask;
+        }
 
         private void DownloadAlbum()
         {
-            string directory = $"{DownloadOptions.AlbumId}";
-            if (!Directory.Exists(directory))
-                Directory.CreateDirectory(directory);
-
+            string directory = TryGetDownloadedFolderName(DownloadOptions.AlbumId);
+            
             PhotoGetParams getParams = new PhotoGetParams()
             {
                 AlbumId = PhotoAlbumType.Id(DownloadOptions.AlbumId),
@@ -64,6 +68,19 @@ namespace VKPhotoAssistant.Utilities.Album.Commands
                 Console.WriteLine($"Файл {photo.Id} начал скачиваться в локальное хранилище");
                 Client.DownloadFile(photo.Sizes.Last().Url, Path.Combine(directory, photo.Id + ".jpg"));
             }
+        }
+
+        private string TryGetDownloadedFolderName(long id)
+        {
+            VkCollection<PhotoAlbum> album = API.Photo.GetAlbums(new PhotoGetAlbumsParams() {
+                AlbumIds = new long[] { DownloadOptions.AlbumId }
+            });
+            string directory = album.First().Title;
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            return directory;
         }
         #endregion
     }
